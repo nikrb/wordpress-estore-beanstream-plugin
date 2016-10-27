9deba0dbd96edaf36c9233a7b0432af83108f2de
nik <nik_rb@yahoo.com>
2016-10-27 19:58:54 +0000

    add cart totals

--- js/esbsBeanstream.js
+++ js/esbsBeanstream.js
@@ -3,5 +3,9 @@ jQuery( document ).ready(function( $ ) {
     ESBS.toggleBeanstreamForm = function(){
         console.log( "toggleBean");
         $( "#beanstream_form").show();
-    }
+        var total_str = $( '.estore-cart-total').find('td:eq(1)').text();
+        var amount = total_str.replace(/[^0-9-.]/g, '');
+        console.log( "cart total[%s] amount:", total_str, amount);
+        $( '#esbs-cart-total').val( amount);
+    };
 });
